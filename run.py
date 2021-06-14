48b23328639cc19a38d362c040cc4ba5f982072e
MAziz-0 <maastudentacc@gmail.com>
2021-06-14 09:05:28 +0000

    Fixed indentation error on line 22

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def services():
     return render_template("services.html")
 
 
-    @app.route("/contact")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
    
