d375e876dc65326f9be659e42ca036b43af24ede
MAziz-0 <maastudentacc@gmail.com>
2021-06-22 06:02:00 +0000

    Added json functionality and a POST method

--- run.py
+++ run.py
@@ -1,73 +1,79 @@
 import os
+import json
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 # Created routing to link Python to html files.
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", page_name = "Home")
+    return render_template("index.html", page_name="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_name = "About Us")
+    data = []
+    with open("data/firm.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_name="About Us", firm=data)
 
 
 @app.route("/services")
 def services():
-    return render_template("services.html", page_name = "Services")
+    return render_template("services.html", page_name="Services")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", page_name = "Contact Us")
+    return render_template("contact.html", page_name="Contact Us")
 
 
 @app.route("/propertylaw")
 def propertylaw():
-    return render_template("property.html", page_name = "Property")
+    return render_template("property.html", page_name="Property")
 
 
 @app.route("/litigation")
 def litigation():
-    return render_template("litigation.html", page_name = "Civil Litigation")
+    return render_template("litigation.html", page_name="Civil Litigation")
 
 
 @app.route("/immigration")
 def immigration():
-    return render_template("immigration.html", page_name = "Home")
+    return render_template("immigration.html", page_name="Home")
 
 
 @app.route("/family")
 def family():
-    return render_template("family.html", page_name = "Family")
+    return render_template("family.html", page_name="Family")
 
 
 @app.route("/wills")
 def wills():
-    return render_template("wills.html", page_name = "Wills & Probate")
+    return render_template("wills.html", page_name="Wills & Probate")
 
 
 @app.route("/employment")
 def employment():
-    return render_template("employment.html", page_name = "Employment")
+    return render_template("employment.html", page_name="Employment")
 
 
 @app.route("/mortgage")
 def mortgage():
-    return render_template("mortgage-info.html", page_name = "How to get a Mortgage in 2021")
+    return render_template("mortgage-info.html", page_name="How to get a Mortgage in 2021")
 
 
 @app.route("/employee")
 def employee():
-    return render_template("employee-info.html", page_name = "Employee Rights")
+    return render_template("employee-info.html", page_name="Employee Rights")
 
 
 @app.route("/tax")
 def tax():
-    return render_template("tax-info.html", page_name = "Capital Gains Tax")
+    return render_template("tax-info.html", page_name="Capital Gains Tax")
 
 
 if __name__ == "__main__":
