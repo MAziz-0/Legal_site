e491be44a7fda088a3692cf77074dee41027a072
MAziz-0 <maastudentacc@gmail.com>
2021-07-19 11:57:24 +0000

    Search function added

--- run.py
+++ run.py
@@ -47,6 +47,13 @@ def task():
     return render_template("myquestions.html", tasks=tasks)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("myquestions.html", tasks=tasks)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
